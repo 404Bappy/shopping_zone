bb79b166d7ace0e4962ba1ca6c863e4d4bec8e1a
Bappy71 <19103076@iubat.edu>
2023-03-05 02:42:22 +0600

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 
 const Header = () => {
     return (
         <div>
             <h1>This the Header !!!</h1>
+            <img src={logo} />
         </div>
     );
 };
