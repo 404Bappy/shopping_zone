4280c5ecef4cd581f27e82e961ef26ce4f108291
Bappy71 <19103076@iubat.edu>
2023-03-10 00:24:09 +0600

    first commitclear

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('./products.JSON')
+    }, [])
     return (
         <div className='shop-container'>
             <div className="product-container">
