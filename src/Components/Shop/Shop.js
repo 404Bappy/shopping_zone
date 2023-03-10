709bfb88822bfd5e32e1a0cd42e249b0157e4d8c
Bappy71 <19103076@iubat.edu>
2023-03-10 16:56:58 +0600

    first commitclear

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -7,11 +7,17 @@ const Shop = () => {
 
     useEffect(() => {
         fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data));
     }, [])
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h3>Products: </h3>
+                <h3>Products:{products.length} </h3>
+                {
+                    products.map(product => console.log(product))
+                }
+
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
