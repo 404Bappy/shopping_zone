b65bb00c7079759c3de2b4eb8cf1776fb9d2e9da
Bappy71 <19103076@iubat.edu>
2023-03-11 03:00:40 +0600

    first commitclear

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,9 +3,18 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product);
+    const { name, img, seller, price, stock } = props.product;
     return (
-        <div>
-            <h1> {props.product.name}</h1>
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+
+            <div>
+                <h4 className='product-name'> {name}</h4>
+                <p><small>By: {seller}</small></p>
+                <p>Price: {price}</p>
+            </div>
 
         </div>
     );
