a13ecae27cdc4c02e4d5d6745a8a262d0dff0fca
Bappy71 <19103076@iubat.edu>
2023-04-27 18:45:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import Header from './Components/Header/Header';
-import Product from './Components/Product/Product';
-import Shop from './Components/Shop/Shop';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Product from "./Components/Product/Product";
+import Shop from "./Components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header />
       <Shop></Shop>
-      
-     
     </div>
   );
 }
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-  
-    const { name, img, seller, price, stock } = props.product;
-    return (
-        <div className='product'>
-            <div>
-                <img src={img} alt="" />
-            </div>
+  const { name, img, seller, price, stock } = props.product;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
 
-            <div>
-                <h4 className='product-name'> {name}</h4>
-                <p><small>By: {seller}</small></p>
-                <p>Price: {price}</p>
-                <p><small>Only {stock} left in stock. Oreder Soon !!</small></p>
-                <button className='btn-cart'>Add to Cart</button>
-            </div>
-
-        </div>
-    );
+      <div>
+        <h4 className="product-name"> {name}</h4>
+        <p>
+          <small>By: {seller}</small>
+        </p>
+        <p>Price: {price}</p>
+        <p>
+          <small>Only {stock} left in stock. Oreder Soon !!</small>
+        </p>
+        <button className="btn-cart">Add to Cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
