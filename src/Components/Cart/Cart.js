29522fd4e715c6307e9e8b73797aac2e9a20429f
Bappy71 <19103076@iubat.edu>
2023-05-04 04:31:41 +0600

    first commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,10 +3,16 @@ import "./Cart.css";
 
 function Cart(props) {
   const { cart } = props;
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+  }
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered:{props.cart.length} </h5>
+      <br />
+      <p>Total: {total} </p>
     </div>
   );
 }
