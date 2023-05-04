ecb4494b299efe947a6e4dffec1a111b65175515
Bappy71 <19103076@iubat.edu>
2023-05-05 02:46:36 +0600

    first commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,12 +7,19 @@ function Cart(props) {
   for (const product of cart) {
     total = total + product.price;
   }
+
+  const Shipping = 15;
+  const tax = (total + Shipping) * 10;
+  const grandTotal = total + Shipping + tax;
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered:{props.cart.length} </h5>
       <br />
-      <p>Total: {total} $ </p>
+      <p>Total: {total.toFixed(2)} $ </p>
+      <p>Shipping: {Shipping} $ </p>
+      <p>Tax: {tax.toFixed(2)} $ </p>
+      <h4 className="GT">Grand Total: {grandTotal.toFixed(2)} $ </h4>
     </div>
   );
 }
